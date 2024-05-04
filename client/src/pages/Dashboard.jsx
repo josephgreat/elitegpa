bacfe34e150ce3432677ebc0ed2c5fbef5ec8658
Joseph <josephgreat2003@gmail.com>
2024-05-04 19:33:10 +0100

    added .gitignore

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Container, Heading } from "@chakra-ui/react";
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Container py="8" maxW="72rem" mx="auto">
+      <Heading textAlign="center" fontSize="clamp(1.5rem, 3vw, 2.5rem)">
+        Dashboard
+      </Heading>
+    </Container>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
