e642ab23dfea965deb2ee7a60f19b35a9ba292ca
Joseph <josephgreat2003@gmail.com>
2024-05-04 19:43:31 +0100

    added server .gitignore

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,33 @@
 import { Container, Heading } from "@chakra-ui/react";
+import { Pie, PieChart } from "recharts";
 import React from "react";
 
 const Dashboard = () => {
+  const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
+    { name: "Group E", value: 278 },
+    { name: "Group F", value: 189 },
+  ];
   return (
     <Container py="8" maxW="72rem" mx="auto">
       <Heading textAlign="center" fontSize="clamp(1.5rem, 3vw, 2.5rem)">
         Dashboard
       </Heading>
+      <PieChart width={700} height={400}>
+        <Pie
+          dataKey="value"
+          isAnimationActive={false}
+          data={data}
+          cx={200}
+          cy={200}
+          outerRadius={80}
+          fill="#8884d8"
+          // label
+        />
+      </PieChart>
     </Container>
   );
 };
