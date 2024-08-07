55972410cbc8ab9530b86724c69ba17ed85db4b6
Joseph <josephgreat2003@gmail.com>
2024-08-07 19:50:51 +0100

    added tls

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGODB_URI;
 console.log(mongoURI);
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tls: true,  // Enforce TLS/SSL connection
+    tlsAllowInvalidCertificates: false,  // Set to true if using self-signed certificates
+  });
 const database = mongoose.connection;
 
 database.on('error',(error) => {
